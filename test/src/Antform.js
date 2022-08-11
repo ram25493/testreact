10877eb83af4b0515f7f9f6ef0699bb6d6c31151
ram25493 <ram.official25@gmail.com>
2022-08-11 16:29:16 +0530

    done

--- test/src/Antform.js
+++ test/src/Antform.js
@@ -1,77 +1,84 @@
-import React from 'react';
-import { Button, Form, Input, InputNumber } from 'antd';
-import 'antd/dist/antd.css';
-
-
+import React from "react";
+import { Button, Form, Input, InputNumber } from "antd";
+import "antd/dist/antd.css";
 
 export default function Antform() {
-  
-      /* eslint-disable no-template-curly-in-string */
-      
-      const validateMessages = {
-        required: '${label} is required!',
-        types: {
-          email: '${label} is not a valid email!',
-          number: '${label} is not a valid number!',
-        },
-        number: {
-          range: '${label} must be between ${min} and ${max}',
-        },
-      };
-      const onFinish = (values) => {
-        console.log(values);
-      };
-    
-      return (
-        <div className='antdform'>
-        <h1 style={{ color: "red", margin: "30px" }}>Form Example</h1>
+  /* eslint-disable no-template-curly-in-string */
 
-        <Form name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-          <Form.Item
-            name={['user', 'name']}
-            label="Name"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name={['user', 'email']}
-            label="Email"
-            rules={[
-              {
-                type: 'email',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name={['user', 'age']}
-            label="Age"
-            rules={[
-              {
-                type: 'number',
-                min: 0,
-                max: 99,
-              },
-            ]}
-          >
-            <InputNumber />
-          </Form.Item>
-          <Form.Item name={['user', 'website']} label="Website">
-            <Input />
-          </Form.Item>
-          <Form.Item name={['user', 'introduction']} label="Introduction">
-            <Input.TextArea />
-          </Form.Item>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-        </Form>
-        </div>
-      );
-    };
+  const validateMessages = {
+    required: "${label} is required!",
+    types: {
+      email: "${label} is not a valid email!",
+      number: "${label} is not a valid number!",
+    },
+    number: {
+      range: "${label} must be between ${min} and ${max}",
+    },
+  };
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="antdform">
+      <h1 style={{ color: "red", margin: "30px" }}>Form Example</h1>
+
+      <Form
+        name="nest-messages"
+        onFinish={onFinish}
+        validateMessages={validateMessages}
+      >
+        <Form.Item
+          name={["user", "name"]}
+          label="Name"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name={["user", "email"]}
+          label="Email"
+          rules={[
+            {
+              type: "email",
+              required: 'true',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name={["user", "age"]}
+          label="Age"
+          rules={[
+            {
+              type: "number",
+              min: 0,
+              max: 99,
+            },
+          ]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item name={["user", "website"]} label="Website"
+        rules={[
+          {
+            required: 'true',
+          },
+        ]}>
+          <Input />
+        </Form.Item>
+        <Form.Item name={["user", "introduction"]} label="Introduction">
+          <Input.TextArea />
+        </Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
